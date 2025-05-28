25322dc2c175b6887185ea4de315d458cebb01f6
Hlungwani masana <hlungwanimasana15@gmail.com>
2025-05-28 09:01:12 +0200

    Restructure: moved React app to client root

--- client/src/api/generateContent.js
+++ client/src/api/generateContent.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function generateContent({ grade, subject, type }) {
-  const response = await axios.post('https://terra-backend3.onrender.com', {
+  const response = await axios.post('https://terra-backend3.onrender.com/generate', {
     grade,
     subject,
     type
